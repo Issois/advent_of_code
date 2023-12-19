c15773688bc1ed767c6ef7a4a3bfda8a1f3c1f34
Issois <30520037+Issois@users.noreply.github.com>
2023-12-19 12:04:49 +0100

    5.1 finished.

--- 05/1.py
+++ 05/1.py
@@ -1,19 +1,54 @@
 
+
 def main():
 
 	with open("input") as f:
 		inp=f.read()
 
-	lines=inp.splitlines()
-	# lines=lines[:2]
-	result=0
-	for idx,line in enumerate(lines):
-		lidx=idx+1
-		win,have=[set([int(y) for y in x.split()]) for x in line.replace("|",":").split(":")[1:]]
-		count=len(win&have)
-		if count>0:
-			result+=2**(count-1)
+	paras=inp.split("\n\n")
 
-	print(result)
+	seeds=[int(x) for x in paras[0].split(": ")[1].split(" ")]
+
+	order=[
+		"seed",
+		"soil",
+		"fertilizer",
+		"water",
+		"light",
+		"temperature",
+		"humidity",
+	]
+
+	maps={}
+
+	for para in paras[1:]:
+		lines=para.split("\n")
+		header=lines[0].split("-")[0]
+		maps[header]=[]
+		for line in lines[1:]:
+			arr=[int(x) for x in line.split(" ")]
+			maps[header].append({"s":arr[1],"d":arr[0],"l":arr[2]})
+
+	locs=[]
+
+	for seed in seeds:
+		print(seed)
+		value=seed
+		for header in order:
+			print(f"- {header} {value}")
+			for mp in maps[header]:
+				source=mp["s"]
+				dest=mp["d"]
+				length=mp["l"]
+				if source<=value<source+length:
+					value=dest+(value-source)
+					# print(f"-- value maps to {value}")
+					break
+		locs.append(value)
+
+
+	print(locs)
+	print(min(locs))
+
 
 main()
