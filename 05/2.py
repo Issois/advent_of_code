9998fca71523e2e36a4786b6ac2350cd474c3458
Issois@Tower23 <notthunder@outlook.com>
2023-12-21 13:04:29 +0100

    cleanup

--- 05/2.py
+++ 05/2.py
@@ -22,11 +22,8 @@ class Rng:
 		return f"{self.start}~{self.length}"
 
 	def __str__(self):
-		# if self.pr_offs and self.offset is not None:
-		# if self.pr_offs:
 		offs=0 if self.offset is None else self.offset
 		return f" {self.start:12}~{str(self.length).ljust(12)} {offs:+13d}   {str(self.offset_name).ljust(20)} {self.name}"
-		# else:
 
 	def apply_offset(self):
 		if self.offset is not None:
@@ -39,11 +36,6 @@ class Rng:
 	def last(self):
 		return self.start+self.length-1
 
-	# def __contains__(self,val):
-	# 	return self.start<=val<self.start+self.length:
-
-	# def is_strict_inside(self,val):
-	# 	return self.start<=val<self.start+self.length:
 	def from_str(text):
 		Rng.INDEX+=1
 		return Rng(f"g{Rng.INDEX}",*[int(x) for x in text.split("~")])
@@ -54,7 +46,6 @@ class Rng:
 		o_stop=rule.stop()
 		if s_stop<=rule.start or self.start>=o_stop:
 			res[1]=self
-			# print(f"-----  NO HIT: rng \"{self.name}\" ({self.rng_str()}), old rule:\"{self.offset_name}\" \"{rule.name}\".")
 			return None
 		else:
 			if self.offset is not None:
@@ -66,17 +57,6 @@ class Rng:
 			res[1]=Rng(f"{self.name}b",max(self.start,rule.start),stop=min(s_stop,o_stop),offset=rule.offset,offset_name=rule.name)
 
 		return [x for x in res if x is not None]
-
-# class LList:
-	# def __init__(self):
-		# self.head=None
-	# def add
-
-# class Element:
-# 	def __init__(self,value):
-# 		self.prev=None
-# 		self.next=None
-# 		self.value=value
 
 def main():
 
@@ -91,7 +71,6 @@ def main():
 
 	if True:
 		with open("input") as f:
-		# with open("input-small") as f:
 			inp=f.read()
 
 		paras=inp.split("\n\n")
@@ -99,11 +78,6 @@ def main():
 		seeds=[int(x) for x in paras[0].split(": ")[1].split(" ")]
 		rngs=[Rng(str((i//2)+1),seeds[i],seeds[i+1]) for i in range(0,len(seeds),2)]
 
-		# for rng in rngs:
-			# print(rng)
-
-		# print(len(seeds))
-		# return
 
 		order=[
 			"seed",
@@ -126,38 +100,6 @@ def main():
 				rule=Rng(f"{header}:{pidx+1}.{lidx+1}",arr[1],arr[2],offset=arr[0]-arr[1])
 				header_rules[header].append(rule)
 
-		# for h,rs in rules.items():
-		# 	print(f"- {h}")
-		# 	for r in rs:
-		# 		print(f" - {r}")
-
-	# rngs=[
-	# 	Rng("1",0,10)
-	# ]
-
-	# rules=[
-	# 	Rng("r1",1,2,offset=10),
-	# 	Rng("r2",5,2,offset=100),
-	# 	Rng("r3",8,1,offset=1000),
-	# ]
-
-	# llist_head=Element(rngs[0])
-	# llist_curr=llist_head
-	# for rng in rngs[1:]:
-	# 	element=Element(rng)
-	# 	llist_curr.next=element
-	# 	element.prev=llist_curr
-	# 	llist_curr=llist_curr.next
-
-
-	# print_llist(llist_curr)
-	# return
-
-
-	# for rng in rngs:
-	# 	rng.length=1
-		# print(f"  {rng}")
-	# for header,rules in header_rules.items():
 	for header in order:
 		rules=header_rules[header]
 		for rule in rules:
@@ -175,42 +117,13 @@ def main():
 					index+=len(elems)
 					rngs.pop(index)
 
-					# print(f" new at {index-len(elems)}-{index}:")
-					# for rng in rngs:
-					# 	print(f"  {rng}")
 		for rng in rngs:
 			rng.apply_offset()
 
-		# print("\n WITH OFFSET")
-		# for rng in rngs:
-		# 	rng.apply_offset()
-		# 	print(f"  {rng}")
-		# 	# print_llist()
 
 	min_locs=sorted([rng.start for rng in rngs])
 	for loc in min_locs:
 		print(loc)
-
-		# break
-	# new_rngs=[]
-	# for rng in rngs:
-	# 	temp_rngs=[rng]
-	# 	for rule in rules:
-	# 		temp_rngs=
-
-	# for
-
-# def print_llist(llist):
-# 	if llist is not None:
-# 		# print(f"llist 0: {llist.value}")
-# 		index=0
-# 		contin=True
-# 		while contin:
-# 			print(f"llist {index}: {llist.value}")
-# 			contin=llist.next is not None
-# 			if contin:
-# 				llist=llist.next
-# 				index+=1
 
 
 def TEST(test):
