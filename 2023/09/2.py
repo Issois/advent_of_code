2603be98a2787458c490b8a6da652ad4e4d233ef
Issois@Tower23 <notthunder@outlook.com>
2023-12-22 16:35:47 +0100

    9.2 finished.

--- 2023/09/2.py
+++ 2023/09/2.py
@@ -1,5 +1,23 @@
+import pandas as pd
+import numpy as np
+
 def main():
-	with open("input") as f:
-		inp=f.readlines()
+	inp=pd.read_csv("input",sep=" ",header=None,index_col=False)
+	inp=np.array(inp)
+
+	# result=inp[:,0]
+	first_cols=[inp[:,0]]
+
+	while np.any(inp):
+		inp=inp[:,1:]-inp[:,:-1]
+		first_cols.append(inp[:,0])
+
+	result=np.zeros(first_cols[0].shape)
+	first_cols=first_cols[::-1]
+	for first_col in first_cols:
+		result=first_col-result
+
+	print(np.sum(result))
+
 
 main()
