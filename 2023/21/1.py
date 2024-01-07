faedb104f7c4cf5a4ac907bba264ca298640e9e6
ack@bgmobile803 <adrian.ciszek@berlinerglas.de>
2024-01-07 15:27:39 +0100

    Work on 20.2.

--- 2023/21/1.py
+++ 2023/21/1.py
@@ -2,7 +2,7 @@
 import numpy as np
 import sys
 def main():
-	with open("example1" if "e" in sys.argv else "input") as f:
+	with open("example1.input" if "e" in sys.argv else "data.input") as f:
 		inp=f.read().split("\n")
 	result=0
 
