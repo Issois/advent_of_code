83a2d1fa202bd05710efc142f117c634ddc6bd3a
ack@bgmobile803 <adrian.ciszek@berlinerglas.de>
2024-04-29 15:29:46 +0200

    Work on 23.23.1.

--- 2023/23/1.py
+++ 2023/23/1.py
@@ -20,6 +20,20 @@ DIRE={
 	WE:np.array([ 0,-1]),
 }
 
+DNAME={
+	NO:"NO",
+	EA:"EA",
+	SO:"SO",
+	WE:"WE",
+}
+
+OPPO={
+	EA:WE,
+	WE:EA,
+	NO:SO,
+	SO:NO,
+}
+
 
 def main():
 	with open(sys.argv[1]) as f:
@@ -58,8 +72,8 @@ def main():
 	pels_to_check=[pel_start]
 	while len(pels_to_check)>0:
 		pel_to_check=pels_to_check.pop(0)
-		for dire in DIRE.values():
-			pos_next=pel_to_check.pos+dire
+		for dire,dire_vec in DIRE.items():
+			pos_next=pel_to_check.pos+dire_vec
 			tpos_next=tuple(pos_next)
 			if is_in_bounds(pos_next,arr) and arr[tpos_next]>FOREST:
 				if not_visited[tpos_next]:
@@ -69,16 +83,15 @@ def main():
 					pels_to_check.append(pel_new)
 					are_neighbors_already=False
 				else:
-					pel_old=pel_from_tpos[tpos_next]
+					pel_prev_visited=pel_from_tpos[tpos_next]
 					are_neighbors_already=False
-					for neigh in pel_old.neighbors:
+					for neigh in pel_prev_visited.neighbors.values():
 						if neigh.id==pel_to_check.id:
 							are_neighbors_already=True
 
 				if not are_neighbors_already:
-					pel_to_check.neighbors.append(pel_new)
-					pel_new.neighbors.append(pel_to_check)
-					# if len([neigh for neigh in pel_old.neighbors if tuple(neigh)])
+					pel_to_check.neighbors[dire] =pel_new
+					pel_new.neighbors[OPPO[dire]]=pel_to_check
 
 	test=np.zeros(arr.shape,dtype=int)
 
@@ -96,67 +109,84 @@ def main():
 			graph[pel.id]=[]
 
 
-	npelids_to_check=[pel_start.id]
+	pelids_checked=set()
+	# print(graph)
+	# return
+
+	npelids_to_check=set([pel_start.id])
 
 	while len(npelids_to_check)>0:
-		return
+		npelid_current=npelids_to_check.pop()
+		npel_current=pel_from_pid[npelid_current]
+		# npel_current_neighbors=npelid_current.neighbors
+
+		for dire_nstart,npel_current_neighbor in npel_current.neighbors.items():
+			print(f"Starting from node at {npel_current.pos} in direction {DNAME[dire_nstart]}.")
+			if npel_current_neighbor.id not in pelids_checked:
+				edge_new=GraphEdge()
+
+				dire_current=dire_nstart
+
+				pel_current=npel_current_neighbor
+				while pel_current.id not in graph:
+					print(f"In direction {DNAME[dire_current]} to pos {pel_current.pos}.")
+					# increas dist
+					edge_new.distance+=1
+					# add to checked
+					pelids_checked.add(pel_current.id)
+
+					assert len(pel_current.neighbors)==2,"Not exactly two neighbors!"
+
+					pel_neighbor_next,dire_next=[(neigh,dire_neigh) for dire_neigh,neigh in pel_current.neighbors.items() if dire_neigh!=OPPO[dire_current]][0]
+
+					# print(neighbor_next,DNAME[dire_next])
+					# return
+					path_current=arr[tuple(pel_current.pos)]
+					if path_current==dire_next:
+						# This is a slope in walking direction.
+						if edge_new.direction=="none":
+							edge_new.direction="forward"
+						assert edge_new.direction!="backward","Edge contains forward and backward."
+					elif path_current==OPPO[dire_current]:
+						# This is a slope against walking direction.
+						if edge_new.direction=="none":
+							edge_new.direction="backward"
+						assert edge_new.direction!="forward","Edge contains forward and backward."
+					dire_current=dire_next
+					pel_current=pel_neighbor_next
+
+				# assert pel_current.id in graph,"End of edge is not a node."
+				# print(pel_current)
+				edge_new.pelid_target=pel_current.id
+				# print(edge_new)
+
+				return
+					# check dire
 
 
-	# graph={}
+				pass
 
-	# for idx,pel in enumerate(pel_from_tpos.values()):
-	# 	test[tuple(pel.pos)]=100
-
-	# for npel in node_pels:
-	# 	test[tuple(npel.pos)]=200
-	# pe=pel_start
-	# test[tuple(pe.pos)]=1
-	# while len(pe.children)>0:
-	# 	pe=pe.children[0]
-	# 	test[tuple(pe.pos)]=1
+		pelids_checked.add(npelid_current)
 
 
+	print(graph)
 
-	plt.imshow(test)
-	plt.show()
-	return
-
-
-	# pos_current=np.array((0,np.nonzero(arr[0])[0][0]))
-	# pos_from_node_id={
-	# 	0:pos_current
-	# }
-	# node_id_from_tpos={tuple(pos):nid for nid,pos in enumerate(pos_from_node_id)}
-
-
-
-	# context={
-	# 	"arr":arr,
-	# 	"not_visited":not_visited,
-	# 	"pos_from_node_id":pos_from_node_id,
-	# 	"node_id_from_tpos":node_id_from_tpos,
-	# }
-	# # print(not_visited.astype(int))
-	# # print(arr)
-
-	# # print(available.astype(int))
-	# # print(start_col)
-	
-	# out={}
-	# id_current=0
-	# len_current=0
-
-	# nodes_and_dires_to_check=[(pos_current,SO)]
-
-
-	# pos_next=
-
-	# while True:
 
 
 
 	answer=0
 	# print(f"ANSWER: {answer}")
+
+class GraphEdge:
+	def __init__(self):
+		self.pelid_target=None
+		self.distance=0
+		self.direction="none"
+	def __str__(self):
+		return f"To pel{self.pelid_target} {self.distance}m {self.direction}"
+	def __repr__(self):
+		return str(self)
+
 
 class PathElement:
 	NEXT_ID=0
@@ -164,9 +194,14 @@ class PathElement:
 		self.id=PathElement.NEXT_ID
 		PathElement.NEXT_ID+=1
 		self.pos=pos
-		self.neighbors=[]
+		self.neighbors={}
 		# self.children=[]
 		# self.parents=[]
+	def __str__(self):
+		return f"pel{self.id}@{self.pos}"
+	def __repr__(self):
+		return str(self)
+
 
 
 def is_in_bounds(pos,arr):
