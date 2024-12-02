b4ef11396ef3c4cb0998c188fa0f2fed918a7370
ack@LT455076 <adrian.ciszek@asml.com>
2024-12-02 07:17:31 +0100

    24.2.2 finished.

--- 2024/02/solve.py
+++ 2024/02/solve.py
@@ -27,7 +27,28 @@ def solve_1(inp):
 	return result
 
 def solve_2(inp):
-	result=None
+	result=0
+	for line in inp:
+		arr=np.array(line.split(" "),dtype=int)
+		if check_line(arr):
+			result+=1
+		else:
+			for idx in range(arr.shape[0]):
+				sub_arr=np.array(list(arr[:idx])+list(arr[idx+1:]))
+				# print(sub_arr)
+				if check_line(sub_arr):
+					result+=1
+					break
 	return result
+
+def check_line(arr):
+	diff=arr[1:]-arr[:-1]
+	pos=diff>0
+	neg=diff<0
+	absd=np.abs(diff)
+	one=absd>=1
+	thr=absd<=3
+	return (np.all(pos) or np.all(neg)) and np.all(one) and np.all(thr)
+
 
 main()
