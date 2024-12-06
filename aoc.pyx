536cc9454407fd9489443e0aad9a83c49bef9b33
Issois@Tower23 <notthunder@outlook.com>
2024-12-06 10:44:48 +0100

    Changed aoc.pyx template.

--- aoc.pyx
+++ aoc.pyx
@@ -21,22 +21,54 @@ NL="\n"
 BOILERPLATE=("""
 import numpy as np
 import sys
-def main():
-	with open(sys.argv[2]) as f:
-		inp=f.read().split("\\n")
-	solve=[solve_1,solve_2]
-	solve=solve[int(sys.argv[1])-1]
+# import matplotlib.pyplot as plt
 
-	print(f"ANSWER: {solve(inp)}")
 
 def solve_1(inp):
-	result=None
-	return result
+	answer=0
+	return answer
 
 def solve_2(inp):
-	result=None
-	return result
+	answer=0
+	return answer
 
+def get_input(file_path):
+	with open(file_path) as f:
+		inp=f.read().split("\\n")
+	return inp
+
+
+def main():
+	inp=get_input(FILE_PATH)
+	answer=solve(inp)
+	print(f"ANSWER: {answer}")
+
+
+
+
+
+RIDDLE_NUMBER=sys.argv[1]
+FILE_PATH=sys.argv[2]
+
+X=0
+Y=1
+Z=2
+
+ROW=0
+COL=1
+
+DIRE=np.array([
+	[-1, 0],
+	[-1, 1],
+	[ 0, 1],
+	[ 1, 1],
+	[ 1, 0],
+	[ 1,-1],
+	[ 0,-1],
+	[-1,-1],
+])
+
+solve=getattr(sys.modules[__name__],"solve_"+RIDDLE_NUMBER)
 main()
 """)
 
