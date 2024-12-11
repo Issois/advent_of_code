6c8395a518808761f1c6da7d12d786368826e9cd
Issois@Tower23 <notthunder@outlook.com>
2024-12-11 15:17:43 +0100

    Finished 24.8.1.

--- 2024/08/solve.py
+++ 2024/08/solve.py
@@ -5,8 +5,53 @@ import sys
 
 
 def solve_1(inp):
+	# pr_inp(inp)
+	inp_cp=inp.copy()
+
 	answer=0
+	nodes=set()
+	antennas=set()
+	for row in inp:
+		antennas.update(row)
+	antennas.remove(".")
+
+	for antenna in antennas:
+		indices=np.array(np.nonzero(inp==antenna)).T
+		# indices=np.nonzero(inp==antenna)
+
+		cnt=indices.shape[0]
+		for a1_iidx in range(cnt):
+			for a2_iidx in range(a1_iidx+1,cnt):
+				a1_idx=indices[a1_iidx]
+				a2_idx=indices[a2_iidx]
+
+				delta=a2_idx-a1_idx
+				p1=a2_idx+delta
+				p2=a1_idx-delta
+
+				for pos in [p1,p2]:
+					if is_in_range(inp,pos):
+						nodes.add(tuple(pos))
+						inp_cp[tuple(pos)]="#"
+
+		# return
+	# print()
+	# pr_inp(inp_cp)
+	answer=len(nodes)
+	# print(antennas)
+
+	# 291
 	return answer
+
+def pr_inp(inp):
+	print("\n".join(["".join(row) for row in inp]))
+
+def is_in_range(arr,pos):
+	return (
+		0<=pos[X]<arr.shape[X]
+		and
+		0<=pos[Y]<arr.shape[Y]
+	)
 
 def solve_2(inp):
 	answer=0
@@ -15,6 +60,7 @@ def solve_2(inp):
 def get_input(file_path):
 	with open(file_path) as f:
 		inp=f.read().split("\n")
+	inp=np.array([list(line) for line in inp])
 	return inp
 
 
