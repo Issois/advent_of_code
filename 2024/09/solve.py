4008ed9f0fffb5c9e7e4d007120b8664e7e0b65a
Issois@Tower23 <notthunder@outlook.com>
2024-12-11 16:17:44 +0100

    Work on 24.9.1.

--- 2024/09/solve.py
+++ 2024/09/solve.py
@@ -6,6 +6,40 @@ import sys
 
 def solve_1(inp):
 	answer=0
+	sizes=inp[::2]
+	spaces=inp[1::2]
+	# print(sizes)
+	# print(spaces)
+	idx_file_start=0
+	idx_inside_start=0
+	idx_file_end=len(sizes)-1
+	idx_inside_end=sizes[idx_file_end]-1
+	idx_space_start=0
+	idx_space_inside_start=0
+	idx_result=0
+	# idx_end=-1
+
+	in_file=True
+
+	result_index=0
+	while True:
+		if in_file:
+			file_index=idx_file_start
+			idx_inside_start+=1
+			if idx_inside_start>=sizes[idx_file_start]:
+				in_file=False
+				idx_file_start+=1
+		else:
+			file_index=idx_file_end
+			idx_space_inside_start+=1
+			if idx_space_inside_start>=spaces[idx_space_start]:
+				in_file=True
+				idx_space_start+=1
+			pass
+
+		answer+=(file_index*result_index)
+		result_index+=1
+
 	return answer
 
 def solve_2(inp):
@@ -14,7 +48,7 @@ def solve_2(inp):
 
 def get_input(file_path):
 	with open(file_path) as f:
-		inp=f.read().split("\n")
+		inp=np.array([int(ch) for ch in f.read()])
 	return inp
 
 
