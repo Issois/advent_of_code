6831e82454bf8aac0e19df1be9b36bec8a1be0af
Issois@Tower23 <notthunder@outlook.com>
2024-12-12 15:34:56 +0100

    Work on 24.9.2.

--- 2024/09/solve.py
+++ 2024/09/solve.py
@@ -11,32 +11,14 @@ def solve_1(inp):
 	answer=0
 	total_length=np.sum(inp)
 	print("len:",total_length)
-	# sections=np.insert(np.cumsum(inp),0,0)
 	sections=np.cumsum(inp)
-	# is_space=np.arange(sections.shape[0])%2==1
-	# is_file=np.logical_not(is_space)
-	# print(is_space)
-	# print(sections)
-	# print(sections[is_space])
-	# print()
-	# result=np.zeros((total_length,2),dtype=int)
-
 
 	backward_index=total_length-1
 	while (backward_section_index:=get_section_index_from_index(backward_index,sections))%2==1:
 		backward_index-=1
 
-
 	for forward_index in range(total_length):
 		forward_section_index=get_section_index_from_index(forward_index,sections)
-		# print(forward_index,)
-		# continue
-		# if forward_index>backward_index:
-			# print(forward_index,backward_index)
-			# break
-		# while forward_index>=sections[forward_section_index]:
-		# 	forward_section_index+=1
-		# print(forward_section_index)
 		is_space=forward_section_index%2==1
 		if backward_index<forward_index:
 			break
@@ -49,21 +31,71 @@ def solve_1(inp):
 		else:
 			file=forward_section_index//2
 		answer+=forward_index*file
-		# result[forward_index,:]=(forward_index,file)
-		# print(f"idx {forward_index} * {file}")
-
-		# index_in_sections=np.nonzero(forward_index<sections)[0][0]-1
-		# print(index_in_sections)
-		# if is
-		# pass
-
 
 	# 6433134381063 is too high.
 	# 6432869891895 is correct.
 	return answer
 
 def solve_2(inp):
+
 	answer=0
+
+	total_length=np.sum(inp)
+	memory=np.zeros((total_length),dtype=int)-1
+	insert_index=0
+	print(inp)
+	for index,length in enumerate(inp):
+		if index%2==0:
+			memory[insert_index:insert_index+length]=index//2
+		insert_index+=length
+
+	memory_str=memory.astype(str)
+	memory_str[memory==-1]=" "
+	print("".join(memory_str))
+
+	spaces=dict(zip(*np.unique(inp,return_counts=True)))
+
+	# return
+
+	# answer=0
+
+	# # spaces_arr=
+
+	# print(spaces)
+	# # for x in :
+	# # 	print(x)
+
+	# total_length=np.sum(inp)
+	# print("len:",total_length)
+	# sections=np.cumsum(inp)
+	# # print(inp)
+	# # return
+	# backward_index=total_length-1
+	# while (backward_section_index:=get_section_index_from_index(backward_index,sections))%2==1:
+	# 	backward_index-=1
+
+	# for forward_index in range(total_length):
+	# 	print(f"{forward_index=}")
+	# 	forward_section_index=get_section_index_from_index(forward_index,sections)
+	# 	is_space=forward_section_index%2==1
+	# 	if backward_index<forward_index:
+	# 		break
+
+	# 	if is_space:
+	# 		file=backward_section_index//2
+	# 		file_len=inp[backward_section_index]
+	# 		if file_len
+	# 		print(file,file_len)
+	# 		return
+	# 		# backward_index-=1
+	# 		# while (backward_section_index:=get_section_index_from_index(backward_index,sections))%2==1:
+	# 			# backward_index-=1
+	# 	else:
+	# 		file=forward_section_index//2
+	# 	answer+=forward_index*file
+
+	# 6433134381063 is too high.
+	# 6432869891895 is correct.
 	return answer
 
 def get_input(file_path):
