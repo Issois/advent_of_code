9702a181e3ecec350959c744df20f169b52c6b86
ack@LT455076 <adrian.ciszek@asml.com>
2024-12-13 10:20:28 +0100

    Finished 24.10.1.

--- 2024/10/solve.py
+++ 2024/10/solve.py
@@ -4,9 +4,43 @@ import sys
 # import matplotlib.pyplot as plt
 
 
-def solve_1(inp):
+def solve_1(arr):
 	answer=0
+	# print(arr)
+	heads=np.array(np.nonzero(arr==0)).T
+
+	for head in heads:
+		positions=[head]
+		peaks=set()
+		while len(positions)>0:
+			position=positions.pop()
+			current_height=arr[tuple(position)]
+			if current_height==9:
+				# print(head,position)
+				# answer+=1
+				peaks.add(tuple(position))
+			else:
+				for dire in DIREV[::2]:
+					new_position=position+dire
+					if is_in_range(new_position,arr):
+						new_height=arr[tuple(new_position)]
+						if new_height-current_height==1:
+							positions.append(new_position)
+		answer+=len(peaks)
+
+	# print(heads)
+
+
 	return answer
+
+# def is_trail(arr,pos,)
+
+def is_in_range(pos,arr):
+	return (
+		0<=pos[X]<arr.shape[X]
+		and
+		0<=pos[Y]<arr.shape[Y]
+	)
 
 def solve_2(inp):
 	answer=0
@@ -15,6 +49,7 @@ def solve_2(inp):
 def get_input(file_path):
 	with open(file_path) as f:
 		inp=f.read().split("\n")
+	inp=np.array([[int(num) for num in row] for row in inp])
 	return inp
 
 
@@ -37,7 +72,7 @@ Z=2
 ROW=0
 COL=1
 
-DIRE=np.array([
+DIREV=np.array([
 	[-1, 0],
 	[-1, 1],
 	[ 0, 1],
